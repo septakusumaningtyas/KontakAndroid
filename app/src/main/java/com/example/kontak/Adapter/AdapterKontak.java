2b38f0bcd70bfe12f4a9d66bb9764f564f330030
septakusumaningtyas <septakusumaningtyas@gmail.com>
2019-11-21 13:30:35 +0700

    Kontak

---
+++ app/src/main/java/com/example/kontak/Adapter/AdapterKontak.java
@@ -0,0 +1,65 @@
+package com.example.kontak.Adapter;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kontak.EditActivity;
+import com.example.kontak.Model.Kontak;
+import com.example.kontak.R;
+
+import java.util.List;
+
+public class AdapterKontak extends RecyclerView.Adapter<AdapterKontak.MyViewHolder>{
+    List<Kontak> mKontakList;
+
+    public AdapterKontak(List <Kontak> KontakList) {
+        mKontakList = KontakList;
+    }
+
+    @NonNull
+    @Override
+    public AdapterKontak.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.kontak_list, parent, false);
+        MyViewHolder mViewHolder = new MyViewHolder(mView);
+        return mViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterKontak.MyViewHolder holder, final int position) {
+        holder.mTextViewId.setText("Id = " + mKontakList.get(position).getId());
+        holder.mTextViewNama.setText("Nama = " + mKontakList.get(position).getNama());
+        holder.mTextViewNomor.setText("Nomor = " + mKontakList.get(position).getNomor());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(view.getContext(), EditActivity.class);
+                mIntent.putExtra("Id", mKontakList.get(position).getId());
+                mIntent.putExtra("Nama", mKontakList.get(position).getNama());
+                mIntent.putExtra("Nomor", mKontakList.get(position).getNomor());
+                view.getContext().startActivity(mIntent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mKontakList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextViewId, mTextViewNama, mTextViewNomor;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextViewId = (TextView) itemView.findViewById(R.id.tvId);
+            mTextViewNama = (TextView) itemView.findViewById(R.id.tvNama);
+            mTextViewNomor = (TextView) itemView.findViewById(R.id.tvNomor);
+        }
+    }
+}
