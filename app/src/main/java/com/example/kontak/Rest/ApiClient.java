06ec4c1b7d51ea47cfb5129c577290f728327ce6
septakusumaningtyas <septakusumaningtyas@gmail.com>
2019-11-28 14:57:24 +0700

    Insert, Update

--- app/src/main/java/com/example/kontak/Rest/ApiClient.java
+++ app/src/main/java/com/example/kontak/Rest/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "http://192.168.67.124/rest_ci/index.php/";
+    public static final String BASE_URL = "http://192.168.67.97/rest_ci/index.php/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit==null) {
